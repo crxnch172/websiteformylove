1e93316015c3b374448b27c166cdddc6900e643e
crxnch172 <cpaulomikhael@gmail.com>
2025-11-14 13:35:36 +0800

    sigma

--- JS/countdown.js
+++ JS/countdown.js
@@ -1,5 +1,5 @@
 // Change the date here using new Date(year, monthIndex (0-11), day, hours, minutes, seconds); month is zero-based (0 = January)
-const anniversaryDate = new Date(2025, 10, 11, 0, 0, 0);
+const anniversaryDate = new Date(2025, 11, 11, 0, 0, 0);
 
 function updateCountdown() {
     const now = new Date();
